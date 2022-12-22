6eeba138cf6ce5a996fa371836be6987fe560a63
itc-s22001 <s22001@std.it-college.ac.jp>
2022-12-22 09:54:27 +0000

    react ミルクティー

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Hero from 'components/hero'
+
+const Home = () => {
+  return (
+    <Hero
+      title='CUBE'
+      subtitle='アウトプットしていくサイト'
+      imageOn
+    />
+  )
+}
+export default Home
